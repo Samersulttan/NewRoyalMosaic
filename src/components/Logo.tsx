2afb6357de9a416041d3a1f6cc66c08144496f6f
Samersulttan <126632770+Samersulttan@users.noreply.github.com>
2025-05-06 12:01:58 +0400

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,15 +3,27 @@ import { Link } from 'react-router-dom';
 const Logo = () => {
   return (
     <Link to="/" className="block">
-      <img
-        src="/images/logo.png"
+      <picture>
+        {/* Modern format - WebP */}
+        <source
+          src="/images/logo.png"
         srcSet="/images/logo.png 1x, /images/logo@2x.png 2x"
-        alt="Royal Mosaic"
-        className="h-20 w-auto object-contain"
-        loading="eager"
-        width="200"
-        height="100"
-      />
+        />
+        {/* Fallback - PNG */}
+        <source
+          srcSet="/images/logo.png 1x, /images/logo@2x.png 2x"
+          type="image/png"
+        />
+        <img
+          src="/images/logo.png"
+          alt="Royal Mosaic"
+          className="h-12 md:h-20 w-auto"
+          width="200"
+          height="100"
+          loading="eager"
+          decoding="async"
+        />
+      </picture>
     </Link>
   );
 };
